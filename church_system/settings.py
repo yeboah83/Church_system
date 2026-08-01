a9ed5d592ee245bbd3fef9ff5d6040193e4a226f
Christian Nana Yeboah <christanyeboah83@gmail.com>
2026-08-01 23:41:05 +0000

    Update settings.py

--- church_system/settings.py
+++ church_system/settings.py
@@ -143,6 +143,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
