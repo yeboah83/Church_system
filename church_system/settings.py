7029a3f10e979816829cd5114c6531d871e258b3
Christian Nana Yeboah <christanyeboah83@gmail.com>
2026-08-02 00:11:42 +0000

    Restrict ALLOWED_HOSTS and add CSRF_TRUSTED_ORIGINS

--- church_system/settings.py
+++ church_system/settings.py
@@ -32,7 +32,13 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-*l6w-8fs!xu(l*b^lqltaryof!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True').lower() in ('true', '1', 't')
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    "school-management-production.up.railway.app",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://school-management-production.up.railway.app",
+]
 
 
 # Application definition
